0d5ff4af60251b32fea6feaa30e67acefcb8fbf1
Rayna Pomakova <rayna.p@sbtech.com>
2017-03-28 16:29:42 +0300

    new name again

--- Automation Course/Homework1/Program.cs
+++ Automation Course/Homework1/Program.cs
@@ -4,8 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Homework1
-{
+namespace DeclareVariables
+    {
     class Program
     {
         static void Main(string[] args)
